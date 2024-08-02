d5ea027f149ac1cfd60a720a7f9322e5e8e18e66
ritika mishra <mritika233@gmail.com>
2024-08-02 11:18:15 +0530

    spec file for controller updated

---
+++ spec/controller/Room_types_controller_spec.rb
@@ -0,0 +1,89 @@
+
+require 'rails_helper'
+
+RSpec.describe RoomTypesController, type: :controller do
+  let(:admin) { create(:user, :admin) }
+  let(:room_type) { create(:room_type) }
+
+  before do
+    sign_in admin
+  end
+
+  describe "GET #index" do
+    it "returns a success response" do
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #show" do
+    it "returns a success response" do
+      get :show, params: { id: room_type.id }
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #new" do
+    it "returns a success response" do
+      get :new
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new RoomType" do
+        expect {
+          post :create, params: { room_type: { name: "Deluxe", rate: 150.0 } }
+        }.to change(RoomType, :count).by(1)
+        expect(response).to redirect_to(room_types_path)
+        expect(flash[:notice]).to eq('Room type was successfully created.')
+      end
+    end
+
+    context "with invalid params" do
+      it "renders the new template" do
+        post :create, params: { room_type: { name: nil, rate: 150.0 } }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns a success response" do
+      get :edit, params: { id: room_type.id }
+      expect(response).to be_successful
+    end
+  end
+
+  describe "PATCH #update" do
+    context "with valid params" do
+      it "updates the room type and redirects to the room types path" do
+        patch :update, params: { id: room_type.id, room_type: { name: "Super Deluxe", rate: 200.0 } }
+        room_type.reload
+        expect(room_type.name).to eq("Super Deluxe")
+        expect(room_type.rate).to eq(200.0)
+        expect(response).to redirect_to(room_types_path)
+        expect(flash[:notice]).to eq('Room type was successfully updated.')
+      end
+    end
+
+    context "with invalid params" do
+      it "renders the edit template" do
+        patch :update, params: { id: room_type.id, room_type: { name: nil, rate: 200.0 } }
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "destroys the room type and redirects to the room types path" do
+      room_type = create(:room_type)
+      expect {
+        delete :destroy, params: { id: room_type.id }
+      }.to change(RoomType, :count).by(-1)
+      expect(response).to redirect_to(room_types_path)
+      expect(flash[:notice]).to eq('Room type was successfully destroyed.')
+    end
+  end
+end
---
+++ spec/controller/Users_cotroller_spec.rb
@@ -0,0 +1,99 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+    let(:admin) { create(:user, :admin, name: "Admin User") }
+    let(:staff) { create(:user, :staff, name: "Staff User") }
+    let(:customer) { create(:user, :customer, name: "Customer User") }
+
+    describe "Get #index" do
+        context "when admin" do
+            before {sign_in admin}
+
+            it "returns a success response" do
+                get :index
+                expect(response).to be_successful
+            end
+        end
+    end
+
+    describe "Get #show" do
+        before { sign_in admin}
+
+        it "return a success response" do
+            get :show, params:{id: customer.id}
+            expect(response).to be_successful
+        end
+    end
+
+    describe "Get #edit" do
+        context "when admin" do
+            before { sign_in admin }
+    
+            it "returns a success response" do
+            get :edit, params: { id: customer.id }
+            expect(response).to be_successful
+            end
+        end
+  
+        context "when not admin" do
+            before { sign_in staff }
+    
+            it "redirects to users path" do
+            get :edit, params: { id: customer.id }
+            expect(response)
+            end
+        end
+    end
+
+    describe "PATCH #update" do
+        context "when admin" do
+            before { sign_in admin }
+
+            it "updates the user and redirects to users path" do
+                patch :update, params: { id: customer.id, user: { role: "staff" } }
+                customer.reload
+                expect(customer.role).to eq("staff")
+                expect(response).to redirect_to(users_path)
+                expect(flash[:notice]).to eq('User was successfully updated.')
+            end
+        end
+
+        context "when not admin" do
+            before { sign_in staff }
+
+            it "does not update the user and redirects to users path" do
+                patch :update, params: { id: customer.id, user: { role: "admin" } }
+                customer.reload
+                expect(customer.role).not_to eq("admin")
+                expect(response)
+            end
+        end
+    end
+
+    describe "DELETE #destroy" do
+        context "when admin" do
+            before { sign_in admin }
+
+            it "destroys the user and redirects to users path" do
+                user = create(:user)
+                expect {
+                delete :destroy, params: { id: user.id }
+                }.to change(User, :count).by(-1)
+                expect(response).to redirect_to(users_path)
+                expect(flash[:notice])==('User was successfully destroyed.')
+            end
+        end
+
+        context "when not admin" do
+            before { sign_in staff }
+
+            it "does not destroy the user and redirects to users path" do
+                user = create(:user)
+                expect {
+                delete :destroy, params: { id: user.id }
+                }
+                expect(response)
+            end
+        end
+    end
+end
