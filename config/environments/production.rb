2be7313c3022ef9b10966395138d7cb30fc4a9e7
ritika mishra <mritika233@gmail.com>
2024-08-02 12:00:21 +0530

    mailconfiguration added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,22 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'no-reply@example.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         'smtp.gmail.com',
+    port:            587,
+    domain:          'example.com',
+    user_name:       'ritikamishra.reet.223@gmail.com',
+    password:        'qlsg wcdo wsiw qzze',
+    authentication:  'plain',
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5 
+  }
 end
